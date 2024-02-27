3371bd39c34e9db881602f5f3d5bf4b53f6cf72d
trinkes <fabiojscosta1583@gmail.com>
2024-02-27 01:48:32 +0000

    add more tests

--- tests/risk_evaluator/rules/test_property_evaluation_rule.py
+++ tests/risk_evaluator/rules/test_property_evaluation_rule.py
@@ -33,3 +33,38 @@ class TestPropertyEvaluation(TestCase):
             property_not_found_value=False,
         )
         self.assertFalse(rule.evaluate({"test_field": "7"}))
+
+    def test_chained_rules(self):
+        rule = PropertyEvaluationRule(
+            property_name="age", operation=operator.lt, value=10
+        ) & PropertyEvaluationRule(
+            property_name="height", operation=operator.le, value=5
+        ) | (
+            PropertyEvaluationRule(
+                property_name="height", operation=operator.gt, value=10
+            )
+            & PropertyEvaluationRule(
+                property_name="age", operation=operator.gt, value=20
+            )
+        )
+        self.assertTrue(rule.evaluate({"age": 5, "height": 5}))
+        self.assertTrue(rule.evaluate({"age": 22, "height": 11}))
+
+        rule = (
+            PropertyEvaluationRule(
+                property_name="age", operation=operator.lt, value=10
+            )  #
+            | PropertyEvaluationRule(
+                property_name="height", operation=operator.le, value=5
+            )  # F
+        ) & (
+            PropertyEvaluationRule(
+                property_name="height", operation=operator.gt, value=10
+            )
+            | PropertyEvaluationRule(
+                property_name="age", operation=operator.gt, value=20
+            )
+        )  # F
+        self.assertFalse(rule.evaluate({"age": 5, "height": 5}))
+        self.assertFalse(rule.evaluate({"age": 22, "height": 11}))
+        self.assertTrue(rule.evaluate({"age": 22, "height": 1}))
